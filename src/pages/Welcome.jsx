3bdf27e9ef669598e8e1afa5f5982615d5c39bc8
tanis27 <154687685+tanis27@users.noreply.github.com>
2025-06-04 20:00:10 -0400

    Finished  welcome page - carousel still needs work

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -12,8 +12,9 @@ function Welcome() {
         <div className='div-centered'>
            <img src={Logo} className='logo' alt="Buzzsmart logo" />
             <Slider />
+            <h1>Ready to Start?</h1>
             <button style={{backgroundColor:'#1c1903',color:'#fffffd'}}className='secondary'>Create Account</button>
-            
+<p>Already a member? <Link>Log in</Link></p>
         </div>);
 }
 export default Welcome
